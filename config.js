9557dc00f84300b82c2a1f56baf2cf1f37892ec3
Arullwara <106657360+Arullwara@users.noreply.github.com>
2022-06-04 13:20:19 +0800

    Update config.js

--- config.js
+++ config.js
@@ -12,14 +12,14 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6281252848955']
+global.owner = ['6285822347348']
 global.premium = ['0']
-global.ownername = 'Zetaa'
-global.botname = 'Zets Public'
-global.packname = '© ZETS - MULTI DEVICE'
-global.gc = 'https://bit.ly/3KMlmv2'
+global.ownername = 'Arull'
+global.botname = 'ArullBotz'
+global.packname = '© Arull - MULTI DEVICE'
+global.gc = 'https://chat.whatsapp.com/LDN0EeJfASs0lbUhJsOJu1'
 global.limitawal = '100'
-global.author = '@naando.jpeg\n@alya.xzy'
+global.author = 'ArullBotz\nArull'
 global.sessionName = 'sessionya'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⌕'
